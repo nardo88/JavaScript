09f0ce5bc5981371e892b7ad109fe67a248a4ca5
nardo88 <nardo1988@mail.ru>
2020-11-23 14:29:10 +0300

    hard 03

---
+++ hard.js
@@ -0,0 +1,76 @@
+// первое задание ==============================================================================
+const ru = ['Воскресение', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+const en = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const date = new Date()
+
+
+
+// через if
+
+const setNameDayWithIf = () => {
+    let lang = prompt('введите идентификатор языка (ru / en) / enter language id (ru / en)')
+    if (lang === 'en'){
+
+        alert('today is ' + en[date.getDay()]);
+
+    } else if (lang === 'ru'){
+
+        alert('сегодня ' + ru[date.getDay()]);
+
+    } else if (lang === null){
+        setNameDayWithIf()
+    }
+    else {
+        setNameDayWithIf()
+    }
+}
+
+// через switch
+const setNameDayWithSwitch = () => {
+    let lang = prompt('введите идентификатор языка (ru / en) / enter language id (ru / en)')
+    switch (lang){
+        case 'en':{
+            alert('today is ' + en[date.getDay()]);
+            break
+        }
+        case 'ru': {
+            alert('сегодня ' + ru[date.getDay()]);
+            break
+        }
+        default:
+            setNameDayWithSwitch() 
+    }
+}
+
+
+// через многомерные массивы
+const arrayDays = [ru, en]
+Array.prototype.ru = [...ru]
+Array.prototype.en = [...en]
+
+const setNameDayWithArray = () => {
+    let lang = prompt('введите идентификатор языка (ru / en) / enter language id (ru / en)')
+
+    lang ? console.log(arrayDays[lang][date.getDay()]) : setNameDayWithArray
+    
+
+    
+}
+
+// setNameDayWithIf()
+// setNameDayWithSwitch() 
+setNameDayWithArray()
+
+
+// второе задание ==================================================================================
+const getYourStatus = () => {
+    let namePerson = prompt('Введите имя')
+
+    namePerson === 'Артем' ? console.log('директор') 
+        : namePerson === 'Максим' ? console.log('Преподаватель')
+        : namePerson === '' ? getYourStatus()
+        : namePerson === null ? getYourStatus()
+        : console.log('Студент'); 
+}
+
+// getYourStatus()
