7ec91a4ac015f20af9332a82481ba7aba0da5a58
nardo88 <nardo1988@mail.ru>
2020-11-24 11:40:12 +0300

    lesson04 hard

--- hard.js
+++ hard.js
@@ -1,73 +1,13 @@
-// первое задание ==============================================================================
+const trimString = (str) => typeof str !== 'string' 
+        ? 'данные не корректны' 
+        : str.length < 30 
+            ? str.trim()
+            : str.trim().substring(0, 29) + '...'
 
 
-const ru = ['Воскресение', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-const en = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+console.log(trimString(' строка '));
+console.log(trimString(10));
 
+let bigStr = '   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea repudiandae hic delectus ut eaque eveniet, optio nisi, quod fugit vero ipsum rerum vel. Reprehenderit commodi suscipit laborum facere. Optio voluptatem numquam eligendi iusto eveniet repellat quaerat sunt quis reiciendis quibusdam!   '
 
-
-// через if
-
-const setNameDayWithIf = () => {
-    let lang = prompt('введите идентификатор языка (ru / en) / enter language id (ru / en)')
-    if (lang === 'en'){
-        console.log(en);
-    } else if (lang === 'ru'){
-
-        console.log(ru);
-
-    } else if (lang === null){
-        setNameDayWithIf()
-    }
-    else {
-        setNameDayWithIf()
-    }
-}
-
-// через switch
-const setNameDayWithSwitch = () => {
-    let lang = prompt('введите идентификатор языка (ru / en) / enter language id (ru / en)')
-    switch (lang){
-        case 'en':{
-            console.log(en);
-            break
-        }
-        case 'ru': {
-            console.log(ru);
-            break
-        }
-        default:
-            setNameDayWithSwitch() 
-    }
-}
-
-
-// через многомерные массивы
-const arrayDays = []
-arrayDays.ru = ru
-arrayDays.en = en
-
-
-const setNameDayWithArray = () => {
-    let lang = prompt('введите идентификатор языка (ru / en) / enter language id (ru / en)')
-    console.log(arrayDays[lang]);
-       
-}
-
-// setNameDayWithIf()
-// setNameDayWithSwitch() 
-setNameDayWithArray()
-
-
-// второе задание ==================================================================================
-const getYourStatus = () => {
-    let namePerson = prompt('Введите имя').toLowerCase()
-
-    namePerson === 'артем' ? console.log('директор') 
-        : namePerson === 'максим' ? console.log('Преподаватель')
-        : namePerson === '' ? getYourStatus()
-        : namePerson === null ? getYourStatus()
-        : console.log('Студент'); 
-}
-
-// getYourStatus()
+console.log(trimString(bigStr));
