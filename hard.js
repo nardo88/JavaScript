b7edeb3032a224181818f229bf3b2cb3575bce6d
nardo88 <nardo1988@mail.ru>
2020-11-24 13:26:57 +0300

    fix

--- hard.js
+++ hard.js
@@ -2,7 +2,7 @@ const trimString = (str) => typeof str !== 'string'
         ? 'данные не корректны' 
         : str.length < 30 
             ? str.trim()
-            : str.trim().substring(0, 29) + '...'
+            : str.trim().substring(0, 30) + '...'
 
 
 console.log(trimString(' строка '));
@@ -11,3 +11,6 @@ console.log(trimString(10));
 let bigStr = '   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea repudiandae hic delectus ut eaque eveniet, optio nisi, quod fugit vero ipsum rerum vel. Reprehenderit commodi suscipit laborum facere. Optio voluptatem numquam eligendi iusto eveniet repellat quaerat sunt quis reiciendis quibusdam!   '
 
 console.log(trimString(bigStr));
+
+// проверка
+console.log(trimString(bigStr).length);
