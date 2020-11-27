4bd49c5da25f984fd6fb92f1379fdfada4bb12cd
nardo88 <nardo1988@mail.ru>
2020-11-27 12:36:34 +0300

    hard mission

---
+++ hard.js
@@ -0,0 +1,16 @@
+const list = document.querySelector('.list');
+const date = new Date();
+const weekDays = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресение'];
+
+weekDays.forEach((item, i) => {
+    let listItem = `<li>${item}</li>`
+    if (item === 'Суббота' || item === 'Воскресение'){
+        listItem = `<li><i>${item}</i></li>`
+    }
+    if (i === date.getDay() - 1){
+        listItem = `<li><b>${item}</b></li>`
+    }
+
+    list.insertAdjacentHTML('beforeend', listItem)
+})
+
