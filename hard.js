f8f91b3474541656766a1f84778ccc6506f13519
nardo88 <nardo1988@mail.ru>
2020-11-27 12:37:15 +0300

    Опять забыл про точки с запятыми

--- hard.js
+++ hard.js
@@ -3,14 +3,14 @@ const date = new Date();
 const weekDays = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресение'];
 
 weekDays.forEach((item, i) => {
-    let listItem = `<li>${item}</li>`
+    let listItem = `<li>${item}</li>`;
     if (item === 'Суббота' || item === 'Воскресение'){
-        listItem = `<li><i>${item}</i></li>`
+        listItem = `<li><i>${item}</i></li>`;
     }
     if (i === date.getDay() - 1){
-        listItem = `<li><b>${item}</b></li>`
+        listItem = `<li><b>${item}</b></li>`;
     }
 
-    list.insertAdjacentHTML('beforeend', listItem)
+    list.insertAdjacentHTML('beforeend', listItem);
 })
 
