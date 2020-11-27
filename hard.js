a3981e8a4390ce184d1309b2c6f6fc9dc036cfd2
nardo88 <nardo1988@mail.ru>
2020-11-27 12:25:48 +0300

    Забыл проставить точки с запятой

---
+++ hard.js
@@ -0,0 +1,4 @@
+const list = document.querySelector('.list')
+
+const weekDays = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресение'];
+
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let isNumber = function (n) {
-    return !isNaN(parseFloat(n)) && isFinite(n)
+    return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 let money;
@@ -11,7 +11,7 @@ let start = function () {
     } while (!isNumber(money))
 }
 
-start()
+start();
 
 // создание объекта
 const appData = {
@@ -40,38 +40,38 @@ const appData = {
         // здесь спросили про депозит в банке
         appData.deposit = confirm('Есть ли у вас депозит в банке?');
         for (let i = 0; i < 2; i++) {
-            let value
-            let key = prompt('Введите обязательную статью расходов?', 'Бензин')
+            let value;
+            let key = prompt('Введите обязательную статью расходов?', 'Бензин');
 
             do {
-               value = +prompt('Во сколько это обойдется?', 3000)
+               value = +prompt('Во сколько это обойдется?', 3000);
             } while (!isNumber(value)) 
 
-            this.expenses[key] = value
+            this.expenses[key] = value;
         }
         
     },
     // метод получения списка обязательных расходов и 
     //суммы денег на эти расходы за месяц
     getExpensesMonth() {
-        let summ = 0
+        let summ = 0;
 
        for (let key in this.expenses){
-           summ += this.expenses[key]
+           summ += this.expenses[key];
        }
-        return summ
+        return summ;
     },
     // метод получения цифры: доход минус расход
     getBudget(){
         // получаем месячный бюджет
-        this.budgetMonth = this.budget - this.expensesMonth
+        this.budgetMonth = this.budget - this.expensesMonth;
         // получаем дневной бюджет
-        this.budgetDay = this.budgetMonth / 30
+        this.budgetDay = this.budgetMonth / 30;
        
     },
     //
     getTargetMonth(){
-        return this.mission / this.budget
+        return this.mission / this.budget;
     },
     getStatusIncome(){
         if (this.budgetDay >= 1200) {
@@ -87,19 +87,19 @@ const appData = {
 
 }
 
-appData.asking()
+appData.asking();
 
 // получение суммы обязательных расходов за месяц
-appData.expensesAmount = appData.getExpensesMonth()
+appData.expensesAmount = appData.getExpensesMonth();
 
 // получение цифры: доход минус расход 
-appData.getBudget()
+appData.getBudget();
 
 console.log('сумма всех обязательных расходов за месяц: ' + appData.expensesAmount);
 
 console.log(Math.floor(appData.getTargetMonth()) < 0 ? 'Цель не будет достигнута' : 'цель будет достигнута через ' + Math.floor(appData.getTargetMonth()) + ' месяцев');
 
-appData.getStatusIncome()
+appData.getStatusIncome();
 
 // вывести все ключи объекта appData
 console.log('Наша программа включает в себя данные: ');
