bb4d6524c39e28ba1d01f7e3c513874f28c12811
nardo88 <nardo1988@mail.ru>
2020-12-02 11:27:29 +0300

    lesson10

--- script.js
+++ script.js
@@ -1,177 +1,43 @@
+const books = document.querySelectorAll('.book');
+const booksWrapper = document.querySelector('.books');
+const body = document.querySelector('body');
+const adv = document.querySelector('.adv')
+const titles = document.querySelectorAll('.books h2 a');
+
+// Восстановить порядок книг.
+booksWrapper.insertAdjacentElement('afterbegin', books[1])
+books[2].replaceWith(books[4])
+booksWrapper.insertAdjacentElement('beforeend', books[2])
+
+// Заменить картинку заднего фона на другую из папки image
+body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
+
+// Исправить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
+titles[4].textContent = 'Книга 3. this и Прототипы Объектов'
+
+// Удалить рекламу со страницы
+adv.remove()
+
+// Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, поможет dev tools)
+// поулчаем список и массив элементов списка
+const listItems2 = books[0].children[1].children
+
+listItems2[1].after(listItems2[3])
+listItems2[2].after(listItems2[6])
+listItems2[3].after(listItems2[8])
+listItems2[4].after(listItems2[6])
+listItems2[9].after(listItems2[6])
+
+const listItems5 = books[5].children[1].children
+listItems5[1].after(listItems5[9])
+listItems5[2].after(listItems5[4])
+listItems5[3].after(listItems5[5])
+listItems5[8].after(listItems5[6])
 
 
-let isNumber = function (n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-let isString = (str) => {
-    if (!parseFloat(str) && str !== null && str.trim() !== ''){
-        return true
-    } else {
-        return false
-    }
-}
-
-let money;
-
-// получаем общий доход за месяц
-let start = function () {
-    while (!isNumber(money)) {
-        money = prompt('Ваш месячный доход?', 75000);
-    } 
-}
-
-start();
-
-// создание объекта
-const appData = {
-    income: {},
-    addIncome: [],
-    expenses: {},
-    // добавление обязательных расходов
-    addExpenses: [],
-    // есть ли депозит в банке
-    deposit: false,
-    // процент депозита
-    percentDeposit: 0,
-    moneyDeposit: 0,
-    // цель - накопить лям
-    mission: 1000000,
-    // за какой период хотим достигнуть цели
-    period: 3,
-    // месячный доход
-    budget: money,
-    // дневной бюджет
-    budgetDay: 0,
-    // бюджет на месяц
-    budgetMonth: 0,
-    // месячные затраты
-    expensesMonth: 0,
-
-    
-    asking() {
-
-        if (confirm('Есть ли у вас дополнительный источник заработка?')){
-            let itemIncome;
-            let cashIncome;
-            do {
-                itemIncome = prompt('Какой у вас дополнительный заработок?', 'таксую');
-            } while (!isString(itemIncome))
-
-            
-            do {
-                cashIncome = prompt('Сколько в месяц вы на этом зарабатываете?', 10000);
-            }while (!isNumber(cashIncome))
-
-            this.income[itemIncome] = cashIncome;
-        }
-
-
-        // здесь спросили про депозит в банке
-        appData.deposit = confirm('Есть ли у вас депозит в банке?');
-        // обязательные расходы
-        for (let i = 0; i < 2; i++) {
-            let value;
-            let key;
-
-            // проверяем что статья расхода - строка
-            do {
-                key = prompt('Введите обязательную статью расходов?', 'Бензин');
-            } while (!isString(key))
-
-            
-            
-            while (!isNumber(value))  {
-               value = prompt('Во сколько это обойдется?', 3000);
-            } 
-
-            this.expenses[key] = value;
-            // заполняем массив addExpenses
-            this.addExpenses.push(key)
-        }
-        
-    },
-    // метод получения списка обязательных расходов и 
-    //суммы денег на эти расходы за месяц
-    getExpensesMonth() {
-
-       for (let key in this.expenses){
-            this.expensesMonth += +this.expenses[key];
-       }
-        
-    },
-    // метод получения цифры: доход минус расход
-    getBudget(){
-        // получаем месячный бюджет
-        this.budgetMonth = this.budget - this.expensesMonth;
-        // получаем дневной бюджет
-        this.budgetDay = this.budgetMonth / 30;
-       
-    },
-    //
-    getTargetMonth(){
-        return this.mission / this.budgetMonth;
-    },
-    getStatusIncome(){
-        if (this.budgetDay >= 1200) {
-            console.log('У вас высокий уровень дохода');
-        } else if (this.budgetDay < 1200 && this.budgetDay > 600) {
-            console.log('У вас средний уровень дохода');
-        } else if (this.budgetDay <= 600) {
-            console.log('К сожалению у вас уровень дохода ниже среднего');
-        } else if (this.budgetDay <= 0) {
-            console.log('Что то пошло не так! Проверьте нет ли под окном коллекторов!');
-        }
-    },
-    getInfoDeposit(){
-        if (this.deposit){
-
-            do {
-                this.percentDeposit = prompt('Какой годовой процент?', '10')
-            } while(!isNumber(this.percentDeposit))
-
-            do {
-                this.moneyDeposit = prompt('Какая сумма заложена?', 10000)
-            } while(!isNumber( this.moneyDeposit))
-            
-        }
-    },
-    calcSavedMoney(){
-       return this.budgetMonth * this.period
-    },
-    getExpenses(){
-        this.addExpenses = this.addExpenses.map(item => {
-            let newItem = item.toLowerCase();
-            let a = newItem.slice(0, 1).toUpperCase();
-            let b = newItem.slice(1);
-            return a + b;
-          
-        });
-        console.log(this.addExpenses.join(', '));
-    }
-
-}
-
-appData.asking();
-
-// получение суммы обязательных расходов за месяц
-appData.getExpensesMonth();
-
-// получение цифры: доход минус расход 
-appData.getBudget();
-
-console.log('сумма всех обязательных расходов за месяц: ' + appData.expensesMonth);
-
-console.log(Math.floor(appData.getTargetMonth()) < 0 ? 'Цель не будет достигнута' : 'цель будет достигнута через ' + Math.floor(appData.getTargetMonth()) + ' месяцев');
-
-appData.getStatusIncome();
-
-// вывести все ключи объекта appData
-console.log('Наша программа включает в себя данные: ');
-for (let key in appData){
-    // console.log('Ключ ' + key + '\n' + 'Значение ' + appData[key]);
-}
-appData.getInfoDeposit()
-console.log(appData.percentDeposit, appData.moneyDeposit, appData.calcSavedMoney());
-
-appData.getExpenses()
+// в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
+const listItems6 = books[2].children[1]
+const liFor6 = document.createElement('li');
+liFor6.classList.add('chapter');
+liFor6.textContent = 'Глава 8: За пределами ES6';
+listItems6.insertAdjacentElement('beforeend', liFor6)
