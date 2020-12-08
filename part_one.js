07263c58fa103181fbef957c4fbc7167bf2b5df8
nardo88 <nardo1988@mail.ru>
2020-12-08 15:51:58 +0300

    fix

--- part_one.js
+++ part_one.js
@@ -1,23 +1,37 @@
-function DomElement(selector, height, width, bg, fontSize, position) {
+
+
+ 
+
+function DomElement(selector, height, width, bg, fontSize, text, position) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
+    this.text = text;
     this.position = position;
 }
 
-DomElement.prototype.createElement = function() {
-    // создаем элемент
-    const elem = document.createElement('div');
+DomElement.prototype.createElement = function () {
+    let elem
+
     // в зависимости от селектора создаем класс или id
-    if(this.selector.slice(0, 1) === '#'){
+
+    if (this.selector.slice(0, 1) === '#') {
+        elem = document.createElement('p');
         elem.id = this.selector.slice(1);
-    } else if (this.selector.slice(0, 1) === '.'){
+    } else if (this.selector.slice(0, 1) === '.') {
+        elem = document.createElement('div');
         elem.className = this.selector.slice(1);
+    } else {
+        
+        elem = null
+
     }
+
     // добавляем стили
-    elem.style.cssText = `
+    if (elem) {
+        elem.style.cssText = `
         height : ${this.height}px;
         width : ${this.width}px;
         background-color : ${this.bg};
@@ -26,17 +40,23 @@ DomElement.prototype.createElement = function() {
         top: 0px;
         left: 0px;
     `;
+        // добавляем етекст
+        elem.textContent = this.text;
+        // добавляем элемент на страницу
+        return elem
+    } else {
+        console.log('указано не корректное значение селектора');
+    }
 
-    return elem
 }
 
 
 
 
-const div1 = new DomElement('#div', 100, 100, 'red', 25, 'absolute')
 
 document.addEventListener('DOMContentLoaded', () => {
-    const elem = div1.createElement()
+    const elem = new DomElement('.div', 100, 100, 'red', 25, '', 'absolute').createElement()
+
     document.body.append(elem)
 
     document.addEventListener('keydown', (event) => {
