76bcf367b708455abbcc4694ac52667606af71de
nardo88 <nardo1988@mail.ru>
2020-12-14 14:36:18 +0300

    lesson18-hard#2

--- script.js
+++ script.js
@@ -1,52 +1,33 @@
-// Введённый в поле текст должен отображаться внутри параграфа,
-// но с задержкой в 300 мс.
+const block = document.querySelector('.block');
+const start = document.querySelector('.start');
+const reset = document.querySelector('.reset');
+const wrapper = document.querySelector('.wrapper');
 
-// При этом каждый введённый пользователем в поле символ сбрасывает 
-// предыдущий отложенный вызов и запускает новый.
+let count = 0;
+let isMove = false;
 
-// Таким образом программа должна ожидать завершения ввода
-// пользователя и только после этого изменять текст в <p></p>
-
-
-const input = document.querySelector('.input');
-const text = document.querySelector('.text');
-
-function inputText(){
-    text.textContent = input.value
+function move() {
+    if (count > Number(wrapper.offsetWidth) - Number(block.offsetWidth)){
+        count = 0;
+    } 
+    count += 5
+    block.style.left = count + 'px';
+    interval = requestAnimationFrame(move)
 }
 
-function throttle(func, ms) {
-
-    let isThrottled = false,
-      savedArgs,
-      savedThis;
-  
-    function wrapper() {
-  
-      if (isThrottled) { 
-        savedArgs = arguments;
-        savedThis = this;
-        return;
-      }
-  
-      func.apply(this, arguments);
-  
-      isThrottled = true;
-  
-      setTimeout(function() {
-
-        isThrottled = false; 
-        
-        if (savedArgs) {
-          wrapper.apply(savedThis, savedArgs);
-          savedArgs = savedThis = null;
-        }
-      }, ms);
+start.addEventListener('click', () => {
+    if (!isMove){
+        interval = requestAnimationFrame(move)
+        isMove = true
+    }  else {
+        cancelAnimationFrame(interval)
+        isMove = false;
     }
-  
-    return wrapper;
-  }
+})
 
-let f1000 = throttle(inputText, 300);
-
-input.addEventListener('input', f1000 )
+reset.addEventListener('click', () => {
+    cancelAnimationFrame(interval);
+    isMove = false;
+    count = 0;
+    block.style.left = count + 'px';
+})
