11d74d99433ea51296775f576ae5b2db633a3b82
nardo88 <nardo1988@mail.ru>
2020-12-23 15:00:02 +0300

    lesson25 fix pattern

--- script/script.js
+++ script/script.js
@@ -404,7 +404,7 @@ window.addEventListener('DOMContentLoaded', () => {
         const validForm1 = new Validator({
             selector: '#form1',
             pattern: {
-                name : /^[а-яА-Я]+$/
+                name : /^[а-яА-Я]+\s+$/
             },
             method: {
                 'form1-phone': [
@@ -431,7 +431,7 @@ window.addEventListener('DOMContentLoaded', () => {
         const validForm2 = new Validator({
             selector: '#form2',
             pattern: {
-                name : /^[а-яА-Я]+$/
+                name : /^[а-яА-Я]+\s+$/
 
             },
             method: {
